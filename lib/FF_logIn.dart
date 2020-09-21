5ce325f56275242cab7fb90db9c12663a1732929
nathan josling <tjc1567@autuni.ac.nz>
2020-09-21 12:45:09 +1200

    Finalized login / signup page

--- lib/FF_logIn.dart
+++ lib/FF_logIn.dart
@@ -1,5 +1,4 @@
 import 'dart:ffi';
-
 import 'package:fitness_freaks/FF_dashboard.dart';
 import 'package:fitness_freaks/FF_heightAndWeight.dart';
 import 'package:flutter/material.dart';
@@ -12,15 +11,189 @@ class FF_logIn extends StatelessWidget {
     Key key,
   }) : super(key: key);
   @override
-
-  //--------------------------------------------------------------Page Properties-----------------------------------------------------------------
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff343a5e),
       body: Stack(
         children: <Widget>[
-          // Transform.translate(
-          //   offset: Offset(64.0, 196.0),
+          SizedBox(
+            width: 275.0,
+            height: 174.0,
+            //--------------------------------------------------------------------Title -------------------------------------------------------------
+            child: Stack(
+              children: <Widget>[
+                Pinned.fromSize(
+                  bounds: Rect.fromLTWH(75.0, 80.0, 275.0, 174.0),
+                  size: Size(275.0, 174.0),
+                  pinLeft: true,
+                  pinRight: true,
+                  pinTop: true,
+                  pinBottom: true,
+                  child: Text(
+                    'FITNESS \n          FREAK',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 41,
+                      color: const Color(0xffffffff),
+                      fontWeight: FontWeight.w900,
+                      shadows: [
+                        Shadow(
+                          color: const Color(0x29000000),
+                          offset: Offset(0, 13),
+                          blurRadius: 6,
+                        )
+                      ],
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(
+            width: 287.0,
+            height: 42.0,
+            child: Stack(
+              children: <Widget>[
+                Pinned.fromSize(
+                  bounds: Rect.fromLTWH(75.0, 410.0, 277.0, 42.0),
+                  size: Size(287.0, 42.0),
+                  pinLeft: true,
+                  pinRight: true,
+                  pinTop: true,
+                  pinBottom: true,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(21.0),
+                      color: const Color(0xff27ae60),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0x29000000),
+                          offset: Offset(0, 8),
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // ----------------------------------------------Rounded buttons Styling-----------------------------------------------
+              ],
+            ),
+          ),
+
+          SizedBox(
+            width: 287.0,
+            height: 41.0,
+            child: Stack(
+              children: <Widget>[
+                Pinned.fromSize(
+                  bounds: Rect.fromLTWH(75.0, 330.0, 270.0, 41.0),
+                  size: Size(287.0, 41.0),
+                  pinLeft: true,
+                  pinRight: true,
+                  pinTop: true,
+                  pinBottom: true,
+                  child: Stack(
+                    children: <Widget>[
+                      Pinned.fromSize(
+                        bounds: Rect.fromLTWH(0.0, 0.0, 287.0, 41.0),
+                        size: Size(287.0, 41.0),
+                        pinLeft: true,
+                        pinRight: true,
+                        pinTop: true,
+                        pinBottom: true,
+                        child: Stack(
+                          children: <Widget>[
+                            Pinned.fromSize(
+                              bounds: Rect.fromLTWH(0.0, 0.0, 290.0, 41.0),
+                              size: Size(287.0, 41.0),
+                              pinLeft: true,
+                              pinRight: true,
+                              pinTop: true,
+                              pinBottom: true,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(21.0),
+                                  color: const Color(0xff2980b9),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color(0x29000000),
+                                      offset: Offset(0, 11),
+                                      blurRadius: 6,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //--------------------------------------------------------------------Sign up button -------------------------------------------------------------
+          Stack(
+            children: [
+              Positioned(
+                  top: 410,
+                  left: 90,
+                  width: 240,
+                  height: 40,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => FF_heightAndWeight()));
+                    },
+                    color: const Color(0xff27ae60),
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 17,
+                        color: const Color(0xffffffff),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ))
+            ],
+          ),
+          //--------------------------------------------------------------------Log in button -------------------------------------------------------------
+          Stack(
+            children: [
+              Positioned(
+                  top: 330,
+                  left: 90,
+                  width: 240,
+                  height: 40,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FF_dashboard()));
+                    },
+                    color: const Color(0xff2980b9),
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 17,
+                        color: const Color(0xffffffff),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ))
+            ],
+          ),
+          //------------------------------------------------------Username box and text input--------------------------------------------------------
           SizedBox(
             width: 287.0,
             height: 41.0,
@@ -43,37 +216,6 @@ class FF_logIn extends StatelessWidget {
               ],
             ),
           ),
-          // ),
-          Stack(
-            children: [
-              Positioned(
-                  top: 10,
-                  left: 70,
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FF_dashboard()));
-                    },
-                    color: const Color(0xff2980b9),
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 17,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w900,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ))
-            ],
-          ),
-          //------------------------------------------------------Username input--------------------------------------------------------
-
-          // Transform.translate(
-          //     offset: Offset(64.0, 196.0),
 
           Stack(
             children: [
@@ -90,6 +232,8 @@ class FF_logIn extends StatelessWidget {
               ),
             ],
           ),
+
+          //------------------------------------------------------Password box and text input--------------------------------------------------------
 
           SizedBox(
             width: 287.0,
@@ -160,9 +304,6 @@ class FF_logIn extends StatelessWidget {
 
           //-----------------------------------------------------Need Help Text-------------------------------------------------------------
 
-          // Transform.translate(
-          //   offset: Offset(156.0, 565.0),
-
           Stack(
             children: [
               Positioned(
@@ -187,141 +328,7 @@ class FF_logIn extends StatelessWidget {
             ],
           ),
 
-          // ),
-
-//--------------------------------------------------------------------Log in button -------------------------------------------------------------
-
-          // Transform.translate(
-          //   offset: Offset(64.0, 364.0),
-          SizedBox(
-            width: 287.0,
-            height: 41.0,
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(75.0, 330.0, 270.0, 41.0),
-                  size: Size(287.0, 41.0),
-                  pinLeft: true,
-                  pinRight: true,
-                  pinTop: true,
-                  pinBottom: true,
-                  child: Stack(
-                    children: <Widget>[
-                      Pinned.fromSize(
-                        bounds: Rect.fromLTWH(0.0, 0.0, 287.0, 41.0),
-                        size: Size(287.0, 41.0),
-                        pinLeft: true,
-                        pinRight: true,
-                        pinTop: true,
-                        pinBottom: true,
-                        child: Stack(
-                          children: <Widget>[
-                            Pinned.fromSize(
-                              bounds: Rect.fromLTWH(0.0, 0.0, 290.0, 41.0),
-                              size: Size(287.0, 41.0),
-                              pinLeft: true,
-                              pinRight: true,
-                              pinTop: true,
-                              pinBottom: true,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(21.0),
-                                  color: const Color(0xff2980b9),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: const Color(0x29000000),
-                                      offset: Offset(0, 11),
-                                      blurRadius: 6,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                // ----------------------------------------------Rounded buttons Styling-----------------------------------------------
-                // Pinned.fromSize(
-                //     bounds: Rect.fromLTWH(110.0, 357.0, 200.0, 21.0),
-                //     size: Size(287.0, 42.0),
-                //     fixedWidth: true,
-                //     fixedHeight: true,
-
-                //  ),
-              ],
-            ),
-          ),
-          // ),
-
-//----------------------------------------------------------------------Sign up button--------------------------------------------------------------
-
-          // Transform.translate(
-          //   offset: Offset(64.0, 469.0),
-          SizedBox(
-            width: 287.0,
-            height: 42.0,
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(75.0, 410.0, 277.0, 42.0),
-                  size: Size(287.0, 42.0),
-                  pinLeft: true,
-                  pinRight: true,
-                  pinTop: true,
-                  pinBottom: true,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(21.0),
-                      color: const Color(0xff27ae60),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color(0x29000000),
-                          offset: Offset(0, 8),
-                          blurRadius: 6,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                // ----------------------------------------------Rounded buttons Styling-----------------------------------------------
-                // Pinned.fromSize(
-                //     bounds: Rect.fromLTWH(110.0, 420.0, 200.0, 21.0),
-                //     size: Size(287.0, 42.0),
-                //     fixedWidth: true,
-                //     fixedHeight: true,
-                Stack(
-                  children: [
-                    Positioned(
-                        child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            new MaterialPageRoute(
-                                builder: (context) => FF_heightAndWeight()));
-                      },
-                      color: const Color(0xff27ae60),
-                      child: const Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 17,
-                          color: const Color(0xffffffff),
-                          fontWeight: FontWeight.w900,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ))
-                  ],
-                )
-              ],
-            ),
-          ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(63.0, 608.0),
+          //--------------------------------------------------------------------Forgot my password -------------------------------------------------------------
           SizedBox(
             width: 285.0,
             height: 41.0,
@@ -391,43 +398,6 @@ class FF_logIn extends StatelessWidget {
                   ),
                 ),
               ],
-            ),
-          ),
-          // ),
-          Transform.translate(
-            offset: Offset(68.0, 43.0),
-            child: SizedBox(
-              width: 275.0,
-              height: 174.0,
-              child: Stack(
-                children: <Widget>[
-                  Pinned.fromSize(
-                    bounds: Rect.fromLTWH(30.0, 30.0, 275.0, 174.0),
-                    size: Size(275.0, 174.0),
-                    pinLeft: true,
-                    pinRight: true,
-                    pinTop: true,
-                    pinBottom: true,
-                    child: Text(
-                      'FITNESS \n          FREAK',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 41,
-                        color: const Color(0xffffffff),
-                        fontWeight: FontWeight.w900,
-                        shadows: [
-                          Shadow(
-                            color: const Color(0x29000000),
-                            offset: Offset(0, 13),
-                            blurRadius: 6,
-                          )
-                        ],
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
-              ),
             ),
           ),
         ],
