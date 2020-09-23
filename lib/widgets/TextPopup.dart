21b676c7c86e2d608ba470dc815d5d3abc5379e7
RubberDuckDestroyer <hitarthasrani@gmail.com>
2020-09-24 00:55:09 +1200

    Added testing examples

--- lib/widgets/TextPopup.dart
+++ lib/widgets/TextPopup.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TextPopup extends StatelessWidget {
-  const TextPopup({Key key, this.title, this.description}) : super(key: key);
+  const TextPopup({Key key, this.context, this.title, this.description})
+      : super(key: key);
 
   // Define parameters required
   final String title;
   final String description;
+  final BuildContext context;
 
   @override
   Widget build(BuildContext context) {
