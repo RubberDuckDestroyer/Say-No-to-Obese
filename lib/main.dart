78e6827962855343b4a696de58882fe8414a6678
Pankaj Prashar <60200280+Prashar-star@users.noreply.github.com>
2020-09-24 22:49:31 +1200

    staged the login button back

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,8 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home: FF_logIn(),
-      home: FF_dietPreferences(),
+      home: FF_logIn(),
+      // home: FF_dietPreferences(),
       // home: FF_resetPassword(),
     );
   }
