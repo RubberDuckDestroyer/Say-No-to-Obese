766d0eb1ab9dca8e24e4e79be7d2e8a653d92846
nathan josling <tjc1567@autuni.ac.nz>
2020-09-25 18:44:53 +1200

    Implemented Slider into design

--- lib/FF_dashboard.dart
+++ lib/FF_dashboard.dart
@@ -26,6 +26,128 @@ class _DashboardPageState extends State<FF_dashboard> {
     return Scaffold(
       backgroundColor: const Color(0xffffffff),
       body: Column(children: [
+        Stack(
+          children: <Widget>[
+            Transform.translate(
+              offset: Offset(0.0, 679.0),
+              child: SizedBox(
+                width: 411.0,
+                height: 52.0,
+                child: Stack(
+                  children: <Widget>[
+                    Pinned.fromSize(
+                      bounds: Rect.fromLTWH(0.0, 0.0, 411.0, 52.0),
+                      size: Size(411.0, 52.0),
+                      pinLeft: true,
+                      pinRight: true,
+                      pinTop: true,
+                      pinBottom: true,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xff343a5e),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(143.0, 679.0),
+              child: Container(
+                width: 125.0,
+                height: 52.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xff262b46),
+                ),
+              ),
+            ),
+            Container(
+              width: 411.0,
+              height: 52.0,
+              decoration: BoxDecoration(
+                color: const Color(0xff343a5e),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(124.0, 14.0),
+              child: SizedBox(
+                width: 164.0,
+                height: 29.0,
+                child: SingleChildScrollView(
+                    child: Text(
+                  'DASHBOARD',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 23,
+                    color: const Color(0xffffffff),
+                    fontWeight: FontWeight.w200,
+                    shadows: [
+                      Shadow(
+                        color: const Color(0x29000000),
+                        offset: Offset(0, 12),
+                        blurRadius: 6,
+                      )
+                    ],
+                  ),
+                  textAlign: TextAlign.left,
+                )),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(357.5, 22.5),
+              child: SizedBox(
+                width: 21.0,
+                height: 12.0,
+                child: Stack(
+                  children: <Widget>[
+                    Pinned.fromSize(
+                      bounds: Rect.fromLTWH(0.0, 0.0, 20.5, 1.0),
+                      size: Size(20.5, 12.3),
+                      pinLeft: true,
+                      pinRight: true,
+                      pinTop: true,
+                      fixedHeight: true,
+                      child: SvgPicture.string(
+                        _svg_2u2hp6,
+                        allowDrawingOutsideViewBox: true,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Pinned.fromSize(
+                      bounds: Rect.fromLTWH(0.0, 6.2, 20.5, 1.0),
+                      size: Size(20.5, 12.3),
+                      pinLeft: true,
+                      pinRight: true,
+                      fixedHeight: true,
+                      child: SvgPicture.string(
+                        _svg_vq5bjh,
+                        allowDrawingOutsideViewBox: true,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Pinned.fromSize(
+                      bounds: Rect.fromLTWH(0.0, 12.3, 20.5, 1.0),
+                      size: Size(20.5, 12.3),
+                      pinLeft: true,
+                      pinRight: true,
+                      pinBottom: true,
+                      fixedHeight: true,
+                      child: SvgPicture.string(
+                        _svg_a0hp8f,
+                        allowDrawingOutsideViewBox: true,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(100.5, 100.5),
+            ),
+          ],
+        ),
         CarouselSlider(
           options: CarouselOptions(
             height: 200.0,
@@ -51,129 +173,6 @@ class _DashboardPageState extends State<FF_dashboard> {
         ),
       ]),
     );
-
-    Stack(
-      children: <Widget>[
-        Transform.translate(
-          offset: Offset(0.0, 679.0),
-          child: SizedBox(
-            width: 411.0,
-            height: 52.0,
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(0.0, 0.0, 411.0, 52.0),
-                  size: Size(411.0, 52.0),
-                  pinLeft: true,
-                  pinRight: true,
-                  pinTop: true,
-                  pinBottom: true,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff343a5e),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Transform.translate(
-          offset: Offset(143.0, 679.0),
-          child: Container(
-            width: 125.0,
-            height: 52.0,
-            decoration: BoxDecoration(
-              color: const Color(0xff262b46),
-            ),
-          ),
-        ),
-        Container(
-          width: 411.0,
-          height: 52.0,
-          decoration: BoxDecoration(
-            color: const Color(0xff343a5e),
-          ),
-        ),
-        Transform.translate(
-          offset: Offset(124.0, 14.0),
-          child: SizedBox(
-            width: 164.0,
-            height: 29.0,
-            child: SingleChildScrollView(
-                child: Text(
-              'DASHBOARD',
-              style: TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 23,
-                color: const Color(0xffffffff),
-                fontWeight: FontWeight.w200,
-                shadows: [
-                  Shadow(
-                    color: const Color(0x29000000),
-                    offset: Offset(0, 12),
-                    blurRadius: 6,
-                  )
-                ],
-              ),
-              textAlign: TextAlign.left,
-            )),
-          ),
-        ),
-        Transform.translate(
-          offset: Offset(357.5, 22.5),
-          child: SizedBox(
-            width: 21.0,
-            height: 12.0,
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(0.0, 0.0, 20.5, 1.0),
-                  size: Size(20.5, 12.3),
-                  pinLeft: true,
-                  pinRight: true,
-                  pinTop: true,
-                  fixedHeight: true,
-                  child: SvgPicture.string(
-                    _svg_2u2hp6,
-                    allowDrawingOutsideViewBox: true,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(0.0, 6.2, 20.5, 1.0),
-                  size: Size(20.5, 12.3),
-                  pinLeft: true,
-                  pinRight: true,
-                  fixedHeight: true,
-                  child: SvgPicture.string(
-                    _svg_vq5bjh,
-                    allowDrawingOutsideViewBox: true,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Pinned.fromSize(
-                  bounds: Rect.fromLTWH(0.0, 12.3, 20.5, 1.0),
-                  size: Size(20.5, 12.3),
-                  pinLeft: true,
-                  pinRight: true,
-                  pinBottom: true,
-                  fixedHeight: true,
-                  child: SvgPicture.string(
-                    _svg_a0hp8f,
-                    allowDrawingOutsideViewBox: true,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Transform.translate(
-          offset: Offset(100.5, 100.5),
-        ),
-      ],
-    );
   }
 }
 
@@ -193,12 +192,12 @@ class Item1 extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text("Data",
+          Text("Workout 1",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 22.0,
                   fontWeight: FontWeight.bold)),
-          Text("Data",
+          Text("Workout 2",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 17.0,
